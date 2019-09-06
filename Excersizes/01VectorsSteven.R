6f5a2b0261009c62f00306c64b0b7258cc097ef6
steven kelty <skelty94@gmail.com>
2019-09-06 15:34:16 -0600

    Added Excersizes and Assignment_3

---
+++ Excersizes/01VectorsSteven.R
@@ -0,0 +1,25 @@
+x <- c(4,6,5,7,10,9,4,15) #### 
+#### c(4,6,5,7,10,9,4,15) < 7
+
+p <- c(3, 5, 6, 8) 
+q <- c(3, 3, 3)
+p + q
+
+
+z <- 0:9 # use alt + minus to make left arrow
+digits <- as.character(z) as.integer(digits)
+#output is a. Object not found
+x <- c(1,2,3,4)
+(x+2)[(!is.na(x)) & x > 0] -> k
+#output is d. 3,4,5,6
+
+#Excersize 6
+s = c("a","b","c","d","e")
+t = c("p","f","g","h","i","j")
+s +3 
+#Error in s + 3 : non-numeric argument to binary operator
+
+#Excersize 7
+v = 1:5 
+z = c(s,v)
+z[5:10]
